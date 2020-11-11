65b57fa989f2a199f45b0b51e44766b4456de4b6
cliljdn <caliljaudiannn@gmail.com>
2020-11-11 23:54:56 +0800

    wip

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,68 +1,66 @@
 <template>
-	<div class="card">
-		<header class="card-header columns level is-mobile">
-			<p class="card-header-title  level-item has-text-centered">
-				Scan Olongapo
-			</p>
-		</header>
-		<section class="mt-6">
-			<h3 class="has-text-centered has-text-weight-semibold is-capitalized ">
-				Providing Security for your information
-			</h3>
-			<p
-				class="has-text-centered has-text-weight-semibold is-capitalized ml-5 mr-5"
-			>
-				Join us in stopping the spread of COVID-19
-			</p>
-			<p class="has-text-centered has-text-weight-semibold ml-5 mr-5">
-				through this community-driven monitoring, <br />
-				<a href="">Download our android application</a>
-			</p>
-		</section>
+	<div class="card-form columns">
+		<div class="card column  is-offset-one-quarter">
+			<header class="level is-mobile">
+				<p class="card-header-title  level-item has-text-centered">
+					Scan Olongapo
+				</p>
+			</header>
+			<section class="mt-3">
+				<h3 class="has-text-centered has-text-weight-semibold is-capitalized ">
+					Providing Security for your information
+				</h3>
+				<p
+					class="has-text-centered has-text-weight-semibold is-justify-content-center is-capitalized ml-5 mr-5"
+				>
+					Join us in stopping the spread of COVID-19 through this
+					community-driven monitoring,
+				</p>
+				<div class="card-content">
+					<div class="content">
+						<form action="" method="POST" @submit.prevent>
+							<label class="" for=""><strong>Email:</strong></label>
+							<div class="field">
+								<div class="control has-icons-left mt-2">
+									<input
+										class="input input-shadow is-hovered"
+										type="email"
+										placeholder="Enter E-mail"
+									/>
+									<span class="icon is-small is-left">
+										<i class="fas fa-envelope"></i>
+									</span>
+								</div>
+							</div>
 
-		<div class="card-content">
-			<div class="content">
-				<form action="" method="POST" @submit.prevent>
-					<label class="" for=""><strong>Email:</strong></label>
-					<div class="field">
-						<div class="control has-icons-left mt-2">
-							<input
-								class="input input-shadow is-hovered"
-								type="email"
-								placeholder="Enter E-mail"
-							/>
-							<span class="icon is-small is-left">
-								<i class="fas fa-envelope"></i>
-							</span>
-						</div>
+							<label class="" for=""><strong>Password:</strong></label>
+							<div class="field">
+								<div class="control has-icons-left mt-2">
+									<input
+										class="input is-hovered"
+										type="password"
+										placeholder="Enter Password"
+									/>
+									<span class="icon is-small is-left">
+										<i class="fas fa-key"></i>
+									</span>
+								</div>
+							</div>
+							<button
+								class="button is-success is-fullwidth is-link is-rounded mt-6"
+							>
+								Login
+							</button>
+
+							<button
+								class="button is-primary is-fullwidth is-link is-rounded mt-6"
+							>
+								Register
+							</button>
+						</form>
 					</div>
-
-					<label class="" for=""><strong>Password:</strong></label>
-					<div class="field">
-						<div class="control has-icons-left mt-2">
-							<input
-								class="input is-hovered"
-								type="password"
-								placeholder="Enter Password"
-							/>
-							<span class="icon is-small is-left">
-								<i class="fas fa-key"></i>
-							</span>
-						</div>
-					</div>
-					<button
-						class="button is-success is-fullwidth is-link is-rounded mt-6"
-					>
-						Login
-					</button>
-
-					<button
-						class="button is-primary is-fullwidth is-link is-rounded mt-6"
-					>
-						Register
-					</button>
-				</form>
-			</div>
+				</div>
+			</section>
 		</div>
 	</div>
 </template>
@@ -78,12 +76,16 @@ export default {}
 html,
 body {
 	height: 100%;
-	background-color: #dadbdf;
 }
 
-.card {
+.card-form {
 	height: 100%;
-	padding-left: 50px !important;
-	padding-right: 50px !important;
+	width: 500px;
+	position: absolute;
+	right: 75px;
+}
+
+.card-body {
+	background-color: lightgrey !important;
 }
 </style>
