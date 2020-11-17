6005e13455af75ea2a9a8c14e8308b0f3d53f12f
cliljdn <caliljaudiannn@gmail.com>
2020-11-17 23:28:02 +0800

    wip dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import PageNotFound from '@/components/Notfound/NotFound'
-import Index from '@/views/Main.views.vue'
 
 //Admin
 
 Vue.use(VueRouter)
 
 const routes = [
-	{
-		path: '/',
-		name: 'Index',
-		component: Index,
-	},
-
 	{ path: '/404', component: PageNotFound },
 	{ path: '*', redirect: '/404' },
 ]
