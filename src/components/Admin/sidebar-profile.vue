145d71086929219c80f3379526c9e4be48e4833d
cliljdn <caliljaudiannn@gmail.com>
2020-11-22 03:18:42 +0800

    wip sidebar content :c

--- src/components/Admin/sidebar-profile.vue
+++ src/components/Admin/sidebar-profile.vue
@@ -1,17 +1,43 @@
 <template>
 	<div class="is-overlay">
-		<section
-			class="hero is-fullheight profile-sidebar  is-pulled-right has-background-white"
+		<main
+			class="hero is-fullheight p-6 profile-sidebar is-pulled-right  has-background-white"
 		>
-			<div class="columns m-5">
-				<div class="column">
-					<strong>Admin Profile</strong>
-					<span class="icon  is-pulled-right">
+			<div class="main-container">
+				<div class="has-text-weight-bold is-flex-direction-row mb-5">
+					Admin Profile
+					<span class="icon is-pulled-right">
 						<a @click="toggleSideBar"><i class="fas fa-times"></i></a>
 					</span>
+					<div class="is-clearfix"></div>
+				</div>
+
+				<div class="container admin-info">
+					<div class="item-1">
+						<img
+							src="https://i.imgur.com/bCOd9N0.jpg"
+							height="102"
+							width="102"
+							alt="admin-image"
+						/>
+					</div>
+					<div class="item-2 ml-2">
+						<span class="icon is-small is-left level is-centered">
+							<i class="fas fa-user"></i>
+							<span class="">Calil Jaudian</span>
+						</span>
+
+						<span
+							class="icon is-mobile admin-email is-small is-left level is-centered"
+						>
+							<i class="fas fa-envelope"></i>
+							<span class="ml-2 pr-10">caliljaudiannn@gmail.com</span>
+						</span>
+					</div>
 				</div>
 			</div>
-		</section>
+		</main>
+		<div class="prevent-overflow">&nbsp;</div>
 	</div>
 </template>
 
