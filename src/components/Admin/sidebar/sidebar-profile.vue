9d8eecdb1bc4a25eda9c1f559fd39fd18e659eb7
cliljdn <caliljaudiannn@gmail.com>
2020-11-23 04:06:08 +0800

    wip

--- src/components/Admin/sidebar/sidebar-profile.vue
+++ src/components/Admin/sidebar/sidebar-profile.vue
@@ -40,13 +40,48 @@
 				</div>
 				<!-- Admin Additional info -->
 				<div class="info-header">
-					<span class="icon is-small personal-info-text is-left level">
+					<span class="icon is-small personal-icon-text is-left level">
 						<i class="fas fa-info"></i>
 						<span class=" ml-2">Personal Information</span>
 					</span>
+					<button class="btn-edit-profile is-medium  button is-ghost">
+						Edit Profile
+					</button>
 				</div>
+
 				<div class="container-info admin-personal-info">
-					fukcing capstone
+					<span class="icon is-small address-icon is-left level">
+						<i class="fas fa-home"></i>
+						<span class="ml-2">Address: </span>
+					</span>
+					<span class="icon is-small barangay-icon is-left level">
+						<i class="fas fa-home"></i>
+						<span class="ml-2">Barangay: </span>
+					</span>
+					<span class="icon is-small city-icon is-left level">
+						<i class="fas fa-home"></i>
+						<span class="ml-2">City: </span>
+					</span>
+					<span class="icon is-small firstname-icon is-left level">
+						<i class="fas fa-home"></i>
+						<span class="ml-2">Firstname: </span>
+					</span>
+					<span class="icon is-small lastname-icon is-left level">
+						<i class="fas fa-home"></i>
+						<span class="ml-2">Lastname: </span>
+					</span>
+					<span class="icon is-small mname-icon is-left level">
+						<i class="fas fa-home"></i>
+						<span class="ml-2">Middlename: </span>
+					</span>
+					<span class="icon is-small age-icon is-left level">
+						<i class="fas fa-home"></i>
+						<span class="ml-2">Age: </span>
+					</span>
+					<span class="icon is-small contact-icon is-left level">
+						<i class="fas fa-phone"></i>
+						<span class="ml-2">Contact Number: </span>
+					</span>
 				</div>
 			</div>
 		</main>
@@ -54,10 +89,10 @@
 </template>
 
 <script>
-import app from './admin-model/sidebar-profile.model.js'
+import app from '../admin-model/sidebar-profile.model.js'
 export default app
 </script>
 
 <style lang="scss">
-@import './sass/sidebar-profile.scss';
+@import '../sass/sidebar-profile.scss';
 </style>
