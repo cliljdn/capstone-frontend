f1dd49c8a11770285cc856bfa255fe883923f826
cliljdn <caliljaudiannn@gmail.com>
2020-11-30 02:01:37 +0800

    wip

--- src/components/Admin/sidebar/edit-profile.vue
+++ src/components/Admin/sidebar/edit-profile.vue
@@ -2,7 +2,7 @@
 	<div class="show-edit">
 		<section class="hero  sidebar-edit is-pulled-right has-background-white">
 			<div class="edit-container">
-				<div class="has-text-weight-bold mb-5">
+				<div class="edit-header has-text-weight-bold mb-5">
 					Edit Profile
 					<span class="edit-icon icon is-pulled-left p-0">
 						<i class="fas fa-edit"></i>
@@ -12,7 +12,12 @@
 						<a @click="toggleEditForm"><i class="fas fa-times"></i></a>
 					</span>
 				</div>
-
+				<div class="tabs edit-tabs">
+					<ul>
+						<li class="is-active"><a>Personal Information</a></li>
+						<li><a>Account information</a></li>
+					</ul>
+				</div>
 				<personal-info />
 			</div>
 		</section>
