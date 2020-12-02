32d6caa99e937f3baa767e0dc457e15454c349f8
cliljdn <caliljaudiannn@gmail.com>
2020-12-03 02:00:59 +0800

    wip modal contents

--- src/components/Admin/profile/user-profile.vue
+++ src/components/Admin/profile/user-profile.vue
@@ -4,7 +4,14 @@
 			<div class="modal-background"></div>
 			<div class="modal-card">
 				<header class="modal-card-head">
-					<p class="modal-card-title">User Profile</p>
+					<p class="modal-card-title">
+						<span class="icon is-small is-left level">
+							<i class="fas fa-user-circle"></i>
+							<span style="white-space: nowrap;" class="admin ml-2"
+								>User Profile</span
+							>
+						</span>
+					</p>
 					<button
 						class="delete"
 						aria-label="close"
@@ -12,7 +19,7 @@
 					></button>
 				</header>
 				<section class="modal-card-body">
-					<div class="columns">
+					<div class="columns is-1-tablet">
 						<div class="column">
 							<article class="media">
 								<div class="media-left">
@@ -24,33 +31,115 @@
 										/>
 									</figure>
 								</div>
-								<div class="media-content">
-									<div class="content user-content">
-										<span class="icon is-small is-left level">
-											<i class="fas fa-user"></i>
-											<span class="admin- ml-2">Name: </span>
-											<span class="admin- ml-2">McDonalds</span>
-										</span>
-									</div>
-									<div class="content user-content">
-										<span class="icon is-small is-left level">
-											<i class="fas fa-user"></i>
-											<span class="admin- ml-2">owner: </span>
-											<span class="admin- ml-2">Christanto Macapagal</span>
-										</span>
-									</div>
-									<div class="content user-content">
-										<span class="icon is-small is-left level">
-											<i class="fas fa-cog"></i>
-											<span class="account-type ml-2">Address: </span>
-											<span class="admin ml-2">Blk 7 Blvd Nationwide</span>
-										</span>
+								<div class="media-content mt-3">
+									<div class="columns user-info">
+										<div class="column">
+											<div class="columns">
+												<div class="column">
+													<span class="icon is-small is-left level">
+														<i class="fas fa-address-card"></i>
+														<span class="admin ml-2">Firstname: </span>
+														<span class="admin ml-2">Calil</span>
+													</span>
+												</div>
+											</div>
+											<div class="columns">
+												<div class="column">
+													<span class="icon is-small is-left level">
+														<i class="fas fa-address-card"></i>
+														<span class="admin ml-2">Middlename: </span>
+														<span class="admin ml-2">Christopher</span>
+													</span>
+												</div>
+											</div>
+
+											<div class="columns">
+												<div class="column">
+													<span class="icon is-small is-left level">
+														<i class="fas fa-address-card"></i>
+														<span class="account-type ml-2">Lastname: </span>
+														<span class="admin ml-2">Jaudian</span>
+													</span>
+												</div>
+											</div>
+										</div>
+
+										<div class="column address-info">
+											<div class="columns">
+												<div class="column">
+													<span class="icon is-small  is-left level">
+														<i class="fas fa-address-card"></i>
+														<span class="admin ml-2">Address:</span>
+
+														<p class="address-field">
+															<span class="admin ml-2"
+																>Blk 7 Lot 16 Stamonica</span
+															>
+														</p>
+													</span>
+												</div>
+											</div>
+											<div class="columns">
+												<div class="column">
+													<span class="icon is-small is-left level">
+														<i class="fas fa-address-card"></i>
+														<span class="admin ml-2">Barangay: </span>
+														<span class="admin ml-2">Sto.thomas</span>
+													</span>
+												</div>
+											</div>
+
+											<div class="columns">
+												<div class="column">
+													<span class="icon is-small is-left level">
+														<i class="fas fa-address-card"></i>
+														<span class="account-type ml-2">City: </span>
+														<span class="admin ml-2">Olongapo City</span>
+													</span>
+												</div>
+											</div>
+										</div>
 									</div>
 								</div>
 							</article>
 						</div>
 					</div>
+					<div class="columns">
+						<div class="column">
+							<div class="tabs is-centered">
+								<ul>
+									<li class="is-active">
+										<a>
+											<span class="icon is-small"
+												><i class="fas fa-car" aria-hidden="true"></i
+											></span>
+											<span>Travel History</span>
+										</a>
+									</li>
+									<li>
+										<a>
+											<span class="icon is-small"
+												><i class="fas fa-building" aria-hidden="true"></i
+											></span>
+											<span>Est. Entered</span>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<div class="columns">
+						<div class="column is-full profile-travel">
+							<div class="travel-dropdown">
+								<div class="travel-content">
+									<h1>gago ka</h1>
+								</div>
+							</div>
+						</div>
+					</div>
 				</section>
+
 				<footer class="modal-card-foot">
 					<button class="button is-success">Save changes</button>
 					<button class="button">Cancel</button>
@@ -64,3 +153,7 @@
 import app from '../admin-model/user-profile.model'
 export default app
 </script>
+
+<style lang="scss">
+@import '../sass/userprofile-modal.scss';
+</style>
