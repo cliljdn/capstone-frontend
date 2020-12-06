911f09b4ff78ef140940f4b40c9e778137addd5b
cliljdn <caliljaudiannn@gmail.com>
2020-12-07 05:42:15 +0800

    wip travel history

--- src/components/Admin/profile/user-profile.vue
+++ src/components/Admin/profile/user-profile.vue
@@ -130,11 +130,21 @@
 					</div>
 
 					<div class="columns">
-						<div class="column is-full profile-travel"></div>
+						<div class="column is-full profile-travel">
+							<button class="collapsible">Open Collapsible</button>
+							<div class="content">
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+									do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+									Ut enim ad minim veniam, quis nostrud exercitation ullamco
+									laboris nisi ut aliquip ex ea commodo consequat.
+								</p>
+							</div>
+						</div>
 					</div>
 				</section>
 
-				<footer class="modal-card-foot ">
+				<footer class="modal-card-foot has-text-centered">
 					<strong>User Profile Monitor</strong>
 				</footer>
 			</div>
