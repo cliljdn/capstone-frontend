38416137b94f8c4a6272c548e4bfed28299aeade
cliljdn <caliljaudiannn@gmail.com>
2020-12-15 19:37:10 +0800

    wip est list emp create

--- src/components/content/mobile/list-est-fillup.vue
+++ src/components/content/mobile/list-est-fillup.vue
@@ -13,18 +13,35 @@
 							General
 						</p>
 						<ul class="menu-list">
-							<li><a>Dashboard</a></li>
-							<li><a>Customers</a></li>
-						</ul>
-					</div>
+							<div class="columns">
+								<div class="column">
+									<span class="icon has-text-info mr-3">
+										<i class="fas fa-university"></i>
+									</span>
+									<span>Est Name:</span>
+									<span class="ml-3">Owner Here</span>
+								</div>
+							</div>
 
-					<div class="box menu-list">
-						<p class="menu-label">
-							General
-						</p>
-						<ul class="menu-list">
-							<li><a>Dashboard</a></li>
-							<li><a>Customers</a></li>
+							<div class="columns">
+								<div class="column">
+									<span class="icon has-text-info mr-3">
+										<i class="fas fa-location-arrow"></i>
+									</span>
+									<span>Est Address:</span>
+									<span class="ml-3">Owner Here</span>
+								</div>
+							</div>
+
+							<div class="columns">
+								<div class="column">
+									<span class="icon has-text-info mr-3">
+										<i class="fas fa-child"></i>
+									</span>
+									<span>Est Owner:</span>
+									<span class="ml-3">Owner Here</span>
+								</div>
+							</div>
 						</ul>
 					</div>
 				</aside>
---
+++ src/components/content/model/list-est-fillup.mode.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+	data() {
+		return {
+            estList: {},
+        },
+	},
+
+	methods: {},
+
+	mounted() {
+		let { state } = this.$store
+		return this.$axios.get(
+			`${state.BASE_URL} /list/account/establishment/profile`,
+			{
+				headers: {
+					Authorization: state.headers.Authorization,
+				},
+			}
+		)
+	},
+}
+</script>
