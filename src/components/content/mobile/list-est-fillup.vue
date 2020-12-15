8d053f7de6f8304f456083191839b9dcaecc7fa9
cliljdn <caliljaudiannn@gmail.com>
2020-12-15 14:36:21 +0800

    wip list est

---
+++ src/components/content/mobile/list-est-fillup.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="card has-equal-height">
+		<div class="card-content">
+			<div class="content">
+				<div class="columns">
+					<div class="column text-welcome">
+						<span>Select a establishment where you work</span>
+					</div>
+				</div>
+				<aside class="menu">
+					<div class="box">
+						<p class="menu-label">
+							General
+						</p>
+						<ul class="menu-list">
+							<li><a>Dashboard</a></li>
+							<li><a>Customers</a></li>
+						</ul>
+					</div>
+
+					<div class="box menu-list">
+						<p class="menu-label">
+							General
+						</p>
+						<ul class="menu-list">
+							<li><a>Dashboard</a></li>
+							<li><a>Customers</a></li>
+						</ul>
+					</div>
+				</aside>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+@import '../sass/list-est-fillup.scss';
+</style>
