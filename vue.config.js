c74663b2db06ff882e58fe8532f1245ad6989a55
cliljdn <caliljaudiannn@gmail.com>
2020-12-23 09:32:53 +0800

    wip

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
 module.exports = {
 	publicPath:
 		process.env.NODE_ENV === 'production' ? '/capstone-frontend/' : '/',
-
-	devServer: {
-		host: '0.0.0.0',
-		hot: true,
-		disableHostCheck: true,
-	},
 }
