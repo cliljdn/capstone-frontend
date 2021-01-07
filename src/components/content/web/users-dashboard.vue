9fcb366d351d5ed5726ce1f6759c7c4928ae6589
cliljdn <caliljaudiannn@gmail.com>
2021-01-07 16:16:33 +0800

    wip

--- src/components/content/web/users-dashboard.vue
+++ src/components/content/web/users-dashboard.vue
@@ -3,7 +3,12 @@
 </template>
 
 <script>
+import TravelHistory from './travel-history'
 export default {
+	components: {
+		'travel-history': TravelHistory,
+	},
+
 	methods: {
 		toggleSideBar() {
 			return this.$store.commit('toggleSideBar')
